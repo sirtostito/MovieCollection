5fea952ac47a03345d242928646cd083a363dc86
BT_2N2_28 <sample@bths.edu>
2024-02-15 13:59:42 -0500

    2/15, 1:59 P.M

---
+++ src/Movie.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.io.IOException;
+import java.io.File;
+public class Movie {
+    private String movieName;
+    private String[] movieCast;
+    private String movieDirector;
+    private String movieOverview;
+    private int runtime;
+    private double userRating;
+
+}
